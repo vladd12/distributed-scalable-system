439f7e5109f6b7448a9625845e3aa669fe1bbb75
vladd12 <vladd12z32@gmail.com>
2024-08-16 23:03:32 +0300

    Merge different structures in one

--- src/config/json/json_field_base.hpp
+++ src/config/json/json_field_base.hpp
@@ -33,50 +33,147 @@ public:
       std::conditional_t<is_array, std::vector<clean_type>, clean_type>>;
 
   // external_type is type for external acces to stored field data
-  template <bool is_array> //
   using external_type = std::conditional_t<is_array, std::vector<clean_type>, clean_type>;
 
 protected:
   stored_type field;
 
 public:
+  /// \brief Default ctor.
   inline json_field_base() noexcept
   {
   }
 
-  inline json_field_base(const stored_type &data) : field(data)
+  /// \brief Copying ctor for external type.
+  inline json_field_base(const external_type &data) : field(data)
   {
   }
 
-  inline json_field_base(stored_type &&data) : field(std::move(data))
+  /// \brief Moving ctor for external type.
+  inline json_field_base(external_type &&data) : field(std::move(data))
   {
   }
 
+  /// \brief Copying ctor for json field structure.
   inline json_field_base(const json_field_base &rhs) //
       noexcept(noexcept(stored_type(std::declval<const stored_type &>())))
       : field(rhs.field)
   {
   }
 
+  /// \brief Moving ctor for json field structure.
   inline json_field_base(json_field_base &&rhs) //
       noexcept(noexcept(stored_type(std::declval<stored_type &&>())))
       : field(std::move(rhs.field))
   {
   }
 
+  /// \brief Copying assignment operator for external type.
+  inline json_field_base &operator=(const external_type &rhs)
+  {
+    field = rhs;
+  }
+
+  /// \brief Moving assignment operator for external type.
+  inline json_field_base &operator=(external_type &&rhs)
+  {
+    field = std::move(rhs);
+  }
+
+  /// \brief Copying assignment operator for json field structure.
   inline json_field_base &operator=(const json_field_base &rhs)
   {
     field = rhs.field;
   }
 
+  /// \brief Moving assignment operator for json field structure.
   inline json_field_base &operator=(json_field_base &&rhs)
   {
     field = std::move(rhs.field);
   }
 
+  /* Required zone */
+
+  template <typename Element = T, std::enable_if_t<!std::is_array_v<Element> && !is_optional, bool> = true> //
+  inline explicit json_field_base(const njson &data) : field(data[field_name])                              //
+  {
+  }
+
+  template <typename Element = T, std::enable_if_t<!std::is_array_v<Element> && !is_optional, bool> = true> //
+  inline explicit json_field_base(njson &&data) : field(data[field_name])                                   //
+  {
+  }
+
+  template <typename Array = T, std::enable_if_t<std::is_array_v<Array> && !is_optional, bool> = true> //
+  inline explicit json_field_base(const njson &data)                                                   //
+  {
+    const auto &array = data[field_name];
+    auto data_begin = array.begin();
+    auto data_end = array.end();
+    field.reserve(data_end - data_begin);
+    for (auto iter = data_begin; iter != data_end; ++iter)
+      field.push_back(std::move(clean_type(*iter)));
+  }
+
+  template <typename Array = T, std::enable_if_t<std::is_array_v<Array> && !is_optional, bool> = true> //
+  inline explicit json_field_base(njson &&data)                                                        //
+  {
+    auto &array = data[field_name];
+    auto data_begin = array.begin();
+    auto data_end = array.end();
+    field.reserve(data_end - data_begin);
+    for (auto iter = data_begin; iter != data_end; ++iter)
+      field.push_back(std::move(clean_type(std::move(*iter))));
+  }
+
+  /* Optional zone */
+
+  template <typename Element = T, std::enable_if_t<!std::is_array_v<Element> && is_optional, bool> = true> //
+  inline explicit json_field_base(const njson &data)                                                       //
+  {
+    if (data.contains(field_name))
+      field = data[field_name];
+  }
+
+  template <typename Element = T, std::enable_if_t<!std::is_array_v<Element> && is_optional, bool> = true> //
+  inline explicit json_field_base(njson &&data)                                                            //
+  {
+    if (data.contains(field_name))
+      field = std::move(data[field_name]);
+  }
+
+  template <typename Array = T, std::enable_if_t<std::is_array_v<Array> && is_optional, bool> = true> //
+  inline explicit json_field_base(const njson &data)                                                  //
+  {
+    if (data.contains(field_name))
+    {
+      const auto &array = data[field_name];
+      auto data_begin = array.begin();
+      auto data_end = array.end();
+      field->reserve(data_end - data_begin);
+      for (auto iter = data_begin; iter != data_end; ++iter)
+        field->push_back(std::move(clean_type(*iter)));
+    }
+  }
+
+  template <typename Array = T, std::enable_if_t<std::is_array_v<Array> && is_optional, bool> = true> //
+  inline explicit json_field_base(njson &&data)                                                       //
+  {
+    if (data.contains(field_name))
+    {
+      auto &array = data[field_name];
+      auto data_begin = array.begin();
+      auto data_end = array.end();
+      field->reserve(data_end - data_begin);
+      for (auto iter = data_begin; iter != data_end; ++iter)
+        field->push_back(std::move(clean_type(std::move(*iter))));
+    }
+  }
+
+  /* Usability zone */
+
   /// \brief Conversion operator for not optional and not array stored data types.
-  template <typename R = external_type<is_array>> //
-  [[nodiscard]] inline operator R() const
+  [[nodiscard]] constexpr inline operator external_type() const
   {
     if constexpr (is_optional)
       field.value();
@@ -85,8 +182,7 @@ public:
   }
 
   /// \brief Conversion operator for not optional and not array stored data types.
-  template <typename R = external_type<is_array>> //
-  [[nodiscard]] inline operator R &()
+  [[nodiscard]] constexpr inline operator external_type &()
   {
     if constexpr (is_optional)
       return field.value();
@@ -95,8 +191,7 @@ public:
   }
 
   /// \brief Conversion operator for not optional and not array stored data types.
-  template <typename R = external_type<is_array>> //
-  [[nodiscard]] inline operator const R &() const
+  [[nodiscard]] constexpr inline operator const external_type &() const
   {
     if constexpr (is_optional)
       field.value();
@@ -114,93 +209,9 @@ public:
 };
 
 template <typename T, auto &name> //
-struct json_field_required : public json_field_base<T, name, true>
-{
-public:
-  typedef json_field_base<T, name, true> base_type;
-
-  template <typename Element = T, std::enable_if_t<!std::is_array_v<Element>, bool> = true> //
-  inline explicit json_field_required(const njson &data)                                    //
-      : base_type(base_type::stored_type(data[base_type::field_name]))                      //
-  {
-  }
-
-  template <typename Element = T, std::enable_if_t<!std::is_array_v<Element>, bool> = true> //
-  inline explicit json_field_required(njson &&data)                                         //
-      : base_type(std::move(base_type::stored_type(data[base_type::field_name])))           //
-  {
-  }
-
-  template <typename Array = T, std::enable_if_t<std::is_array_v<Array>, bool> = true> //
-  inline explicit json_field_required(const njson &data)                               //
-  {
-    const auto &array = data[base_type::field_name];
-    auto data_begin = array.begin();
-    auto data_end = array.end();
-    this->field.reserve(data_end - data_begin);
-    for (auto iter = data_begin; iter != data_end; ++iter)
-      this->field.push_back(std::move(base_type::element_type(*iter)));
-  }
-
-  template <typename Array = T, std::enable_if_t<std::is_array_v<Array>, bool> = true> //
-  inline explicit json_field_required(njson &&data)                                    //
-  {
-    auto &array = data[base_type::field_name];
-    auto data_begin = array.begin();
-    auto data_end = array.end();
-    this->field.reserve(data_end - data_begin);
-    for (auto iter = data_begin; iter != data_end; ++iter)
-      this->field.push_back(std::move(base_type::element_type(std::move(*iter))));
-  }
-};
+using json_field_required = json_field_base<T, name, true>;
 
 template <typename T, auto &name> //
-struct json_field_optional : public json_field_base<T, name, false>
-{
-public:
-  typedef json_field_base<T, name, true> base_type;
-
-  template <typename Element = T, std::enable_if_t<!std::is_array_v<Element>, bool> = true> //
-  inline explicit json_field_optional(const njson &data)                                    //
-  {
-    if (data.contains(base_type::field_name))
-      this->field = data[base_type::field_name];
-  }
-
-  template <typename Element = T, std::enable_if_t<!std::is_array_v<Element>, bool> = true> //
-  inline explicit json_field_optional(njson &&data)                                         //
-  {
-    if (data.contains(base_type::field_name))
-      this->field = std::move(data[base_type::field_name]);
-  }
-
-  template <typename Array = T, std::enable_if_t<std::is_array_v<Array>, bool> = true> //
-  inline explicit json_field_optional(const njson &data)                               //
-  {
-    if (data.contains(base_type::field_name))
-    {
-      const auto &array = data[base_type::field_name];
-      auto data_begin = array.begin();
-      auto data_end = array.end();
-      this->field->reserve(data_end - data_begin);
-      for (auto iter = data_begin; iter != data_end; ++iter)
-        this->field->push_back(std::move(base_type::element_type(*iter)));
-    }
-  }
-
-  template <typename Array = T, std::enable_if_t<std::is_array_v<Array>, bool> = true> //
-  inline explicit json_field_optional(njson &&data)                                    //
-  {
-    if (data.contains(base_type::field_name))
-    {
-      auto &array = data[base_type::field_name];
-      auto data_begin = array.begin();
-      auto data_end = array.end();
-      this->field->reserve(data_end - data_begin);
-      for (auto iter = data_begin; iter != data_end; ++iter)
-        this->field->push_back(std::move(base_type::element_type(std::move(*iter))));
-    }
-  }
-};
+using json_field_optional = json_field_base<T, name, false>;
 
 } // namespace json
